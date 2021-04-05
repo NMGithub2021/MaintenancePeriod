9e68b5b550415a613744e852ccae284159f28e06
NMGithub2021 <81959011+NMGithub2021@users.noreply.github.com>
2021-04-05 15:56:18 +0300

    сщььше

--- Index.php
+++ Index.php
@@ -1,11 +1,11 @@
 <?php
 
-<<<<<<< HEAD
+
 TEST STRING
 
 =======
 include_once('core/system.php');
->>>>>>> 8b7d6bf9adf668612075fe07dc750646ceae35c9
+
 
 $cname = $_GET['c'] ?? 'index';
 $patch = "controllers/$cname.php";
