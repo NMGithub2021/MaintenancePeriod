8b7d6bf9adf668612075fe07dc750646ceae35c9
Dmytro Nezhelskyi <dimores95@rambler.ru>
2021-04-05 15:38:26 +0300

    second commit

--- Index.php
+++ Index.php
@@ -1,4 +1,25 @@
 <?php
 
+include_once('core/system.php');
 
+$cname = $_GET['c'] ?? 'index';
+$patch = "controllers/$cname.php";
+$pageTitle = 'Ошибка 404';
+$pageContent = '';
+
+
+
+if(checkContrillerName($cname) && file_exists($patch)){
+    include_once($patch);
+}
+else{
+    $pageContent = template('errors/v_404');
+}
+
+$html = template('base/v_main', [
+    'title' => $pageTitle,
+    'content' => $pageContent
+]);
+
+echo $html;
 
